5244f627dbdb056a42f359cb14e1b9248c8eebfa
cosmos <91248142+cosmos1721@users.noreply.github.com>
2024-03-04 00:00:51 +0530

    tt

--- backend/tt.py
+++ backend/tt.py
@@ -1,8 +1,11 @@
-import uvicorn
-from main import app
+import subprocess
+
+def run_command(command):
+    subprocess.run(command, shell=True)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    command = 'chmod +x tt.py && uvicorn main:app --reload --host 0.0.0.0'
+    run_command(command)
 
 
 # chmod +x tt.py && nohup python3 tt.py &
