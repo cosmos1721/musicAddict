219059e46a3a88cee7b6f0aec7370e0ad6b51580
Devesh Sharma <91248142+cosmos1721@users.noreply.github.com>
2024-03-04 21:51:14 +0530

    Delete backend/restart/cron.py

--- backend/restart/cron.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.get("https://musicaddict.onrender.com/")
-        # response = requests.get("http://127.0.0.1:8000/")
-        # Do something with the response if needed
-print("repeat server" + response.text)
-
-# git add .; git commit -m "repeat-tempServer"; git push origin 
